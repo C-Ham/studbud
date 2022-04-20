d616dc416be135d384a18b8918e667ef847e7922
Neesha Chockalingam <neesha.chockalingam@gmail.com>
2022-04-20 18:29:52 +1000

    Finalised active and hover states for navigation links

--- dist/index.f3bd186e.js
+++ dist/index.f3bd186e.js
@@ -1,9 +1,8 @@
 function makeActive(link_id) {
     var sidenavList = document.getElementsByClassName("active");
-    for (let item of sidenavList) {
-        item.classList.remove("active");
-    }
-    
+    for (let item of sidenavList)item.classList.remove("active");
     var sidenavLink = document.getElementById(link_id);
     sidenavLink.classList.add("active");
- }
+}
+
+//# sourceMappingURL=index.f3bd186e.js.map
