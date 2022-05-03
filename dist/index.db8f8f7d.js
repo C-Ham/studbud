b3ca83333f5c2f8cc266e73c727cc4f4535ccdeb
Neesha Chockalingam <neesha.chockalingam@gmail.com>
2022-05-03 15:48:57 +1000

    Fully functioning drag and drop between columns. Tasks disappear when dragged onto non-droppable targets, need to fix that.

--- dist/index.db8f8f7d.js
+++ dist/index.db8f8f7d.js
@@ -4,31 +4,35 @@
         e.target.classList.add('hide');
     }, 0);
 }
-/* drop targets */ let columns = document.querySelectorAll('.kanban__column');
+/* drop targets */ let columns = document.querySelectorAll('.kanban__column-items');
 columns.forEach((col)=>{
     col.addEventListener('dragenter', dragEnter);
     col.addEventListener('dragover', dragOver);
     col.addEventListener('dragleave', dragLeave);
-    col.addEventListener('drop', drop);
+//col.addEventListener('drop', drop);
 });
-function dragEnter(e) {
+function dragEnter(e, el) {
     e.preventDefault();
-    e.target.classList.add('drag-over');
+    el.parentElement.classList.add('drag-over');
+//e.target.classList.add('drag-over');
 }
-function dragOver(e) {
+function dragOver(e, el) {
     e.preventDefault();
-    e.target.classList.add('drag-over');
+    el.parentElement.classList.add('drag-over');
+//e.target.classList.add('drag-over');
 }
-function dragLeave(e) {
-    e.target.classList.remove('drag-over');
+function dragLeave(e, el) {
+    el.parentElement.classList.remove('drag-over');
+//e.target.classList.remove('drag-over');
 }
-function drop(e) {
-    e.target.classList.remove('drag-over');
+function drop(e, el) {
+    el.parentElement.classList.remove('drag-over');
+    //e.target.classList.remove('drag-over');
     // get the draggable element
     const id = e.dataTransfer.getData('text/plain');
     const draggable = document.getElementById(id);
     // add it to the drop target
-    e.target.appendChild(draggable);
+    el.appendChild(draggable);
     // display the draggable element
     draggable.classList.remove('hide');
 }
