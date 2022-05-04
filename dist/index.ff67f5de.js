b502f98a0c92c8363769134c4ff21c9ee3d67a5d
Neesha Chockalingam <neesha.chockalingam@gmail.com>
2022-05-04 14:19:42 +1000

    Fixed datalist prematurely closing modal on enter, and moved several functions to modal.js file

---
+++ dist/index.ff67f5de.js
@@ -0,0 +1,36 @@
+//Fetch form elements to use for future addTask() calls
+const taskTitle = document.getElementById("task-title");
+const taskDesc = document.getElementById("task-desc");
+const priority = document.getElementById("priority");
+const category = document.getElementById("category");
+const categories = document.getElementById("categories");
+const dueDate = document.getElementById("due-date");
+const completionTime = document.getElementById("completion-time");
+category.addEventListener("keypress", function(e) {
+    if (e.key === "Enter") e.preventDefault();
+});
+//Triggered when submit button is clicked on the "Add Task" Modal
+function onSubmit() {
+    //Form validation
+    //Title and description missing
+    if (!taskTitle.value && !taskDesc.value) {
+        taskTitle.style.border = "2px solid #FF5630";
+        taskDesc.style.border = "2px solid #FF5630";
+    } else if (!taskTitle.value) {
+        taskTitle.style.border = "2px solid #FF5630";
+        taskDesc.style.border = "0px solid";
+    } else if (!taskDesc.value) {
+        taskTitle.style.border = "0px solid";
+        taskDesc.style.border = "2px solid #FF5630";
+    } else {
+        //Add task to DOM
+        addTask(taskTitle.value, taskDesc.value, priority.value, category.value, dueDate.value, completionTime.value);
+        //Reset form elements in case their styles were altered from validation checks
+        taskTitle.style.border = "0px solid";
+        taskDesc.style.border = "0px solid";
+        document.getElementById("addtask").reset();
+        closeModal();
+    }
+}
+
+//# sourceMappingURL=index.ff67f5de.js.map
---
+++ public/scripts/modal.js
@@ -0,0 +1,50 @@
+//Fetch form elements to use for future addTask() calls
+const taskTitle = document.getElementById("task-title");
+const taskDesc = document.getElementById("task-desc");
+const priority = document.getElementById("priority");
+const category = document.getElementById("category");
+const categories = document.getElementById("categories");
+const dueDate = document.getElementById("due-date");
+const completionTime = document.getElementById("completion-time");
+
+category.addEventListener("keypress", function(e) {
+    if (e.key === "Enter") {
+      e.preventDefault();
+    }
+});
+
+//Triggered when submit button is clicked on the "Add Task" Modal
+function onSubmit() {
+  //Form validation
+  //Title and description missing
+  if(!taskTitle.value && !taskDesc.value) {
+    taskTitle.style.border = "2px solid #FF5630";
+    taskDesc.style.border = "2px solid #FF5630";
+
+  }
+
+  //Title missing
+  else if(!taskTitle.value) {
+    taskTitle.style.border = "2px solid #FF5630";
+    taskDesc.style.border = "0px solid";
+  }
+
+  //Description missing
+  else if(!taskDesc.value) {
+    taskTitle.style.border = "0px solid";
+    taskDesc.style.border = "2px solid #FF5630";
+  }
+
+  //Successful submission
+  else {
+    //Add task to DOM
+    addTask(taskTitle.value, taskDesc.value, priority.value, category.value, dueDate.value, completionTime.value);
+
+    //Reset form elements in case their styles were altered from validation checks
+    taskTitle.style.border = "0px solid";
+    taskDesc.style.border = "0px solid";
+    document.getElementById("addtask").reset();
+
+    closeModal();
+  }
+}
