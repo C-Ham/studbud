00f55ffd48cca11de01e35519ebafb532d4a9a31
Neesha Chockalingam <neesha.chockalingam@gmail.com>
2022-05-09 17:41:30 +1000

    Mostly functioning timer UI, with flexible countdown and progress bar animations. Need to fix small issue that offsets stroke-dashoffset at an incorrect rate on page reload

--- dist/pomodoro.24d87240.js
+++ dist/pomodoro.24d87240.js
@@ -1,9 +1,16 @@
 const playBtn = document.getElementById("play-icon");
 const pauseBtn = document.getElementById("pause-icon");
 const countdownTimer = document.getElementById("time-remaining");
-const studyMinutes = 25;
+const progressCircle = document.getElementById("progress-circle");
+const studyMinutes = 1;
 let studyTime = studyMinutes * 60;
+renderTime(Math.floor(studyTime / 60), studyTime % 60);
 var timerStart;
+function makeActiveSession(session) {
+    var sessionList = document.getElementsByClassName("pomodoro-buttons");
+    for (let item of sessionList)item.classList.remove("active-state");
+    session.classList.add("active-state");
+}
 function togglePlayPause(btn) {
     if (btn.id == "play-icon") {
         playBtn.classList.add("hidden");
@@ -15,17 +22,33 @@ function togglePlayPause(btn) {
         clearInterval(timerStart);
     }
 }
+function resetTimer() {
+    progressCircle.style.strokeDashoffset = 0;
+    countdownTimer.innerHTML = "0:00";
+    studyTime = studyMinutes * 60;
+    clearInterval(timerStart);
+    pauseBtn.classList.add("hidden");
+    playBtn.classList.remove("hidden");
+}
 function updateCountdown() {
     const minutes = Math.floor(studyTime / 60);
     let seconds = studyTime % 60;
+    if (studyTime == 0) resetTimer();
+    else {
+        renderTime(minutes, seconds);
+        studyTime--;
+        progressCircle.style.strokeDashoffset = percentToOffset(timeToPercent());
+    }
+}
+function renderTime(minutes, seconds) {
     seconds = seconds < 10 ? '0' + seconds : seconds;
     countdownTimer.innerHTML = minutes + ":" + seconds;
-    studyTime--;
 }
-function makeActiveSession(session) {
-    var sessionList = document.getElementsByClassName("pomodoro-buttons");
-    for (let item of sessionList)item.classList.remove("active");
-    session.classList.add("active");
+function timeToPercent() {
+    return 1 - studyTime / (studyMinutes * 60);
+}
+function percentToOffset(percent) {
+    return 1159 - 1159 * percent;
 }
 
 //# sourceMappingURL=pomodoro.24d87240.js.map
--- public/scripts/pomodoro.js
+++ public/scripts/pomodoro.js
@@ -1,11 +1,22 @@
 const playBtn = document.getElementById("play-icon");
 const pauseBtn = document.getElementById("pause-icon");
 const countdownTimer = document.getElementById("time-remaining");
+const progressCircle = document.getElementById("progress-circle");
 
-const studyMinutes = 25;
+const studyMinutes = 1;
 let studyTime = studyMinutes * 60;
+renderTime(Math.floor(studyTime / 60), studyTime % 60);
 
 var timerStart;
+
+function makeActiveSession(session) {
+    var sessionList = document.getElementsByClassName("pomodoro-buttons");
+    for (let item of sessionList) {
+        item.classList.remove("active-state");
+    }
+    
+    session.classList.add("active-state");
+}
 
 function togglePlayPause(btn) {
     if (btn.id == "play-icon") {
@@ -20,20 +31,40 @@ function togglePlayPause(btn) {
     }
 }
 
+function resetTimer() {
+    progressCircle.style.strokeDashoffset = 0;
+    countdownTimer.innerHTML = "0:00";
+    studyTime = studyMinutes * 60;
+    clearInterval(timerStart);
+    pauseBtn.classList.add("hidden");
+    playBtn.classList.remove("hidden");
+}
+
 function updateCountdown() {
     const minutes = Math.floor(studyTime / 60);
     let seconds = studyTime % 60;
 
-    seconds = seconds < 10 ? '0' + seconds : seconds;
-    countdownTimer.innerHTML = minutes + ":" + seconds;
-    studyTime--;
+
+    if (studyTime == 0) {
+        resetTimer();
+    }
+
+    else {
+        renderTime(minutes, seconds);
+        studyTime--;
+        progressCircle.style.strokeDashoffset = percentToOffset(timeToPercent());
+    }
 }
 
-function makeActiveSession(session) {
-    var sessionList = document.getElementsByClassName("pomodoro-buttons");
-    for (let item of sessionList) {
-        item.classList.remove("active");
-    }
-    
-    session.classList.add("active");
+function renderTime(minutes, seconds) {
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    countdownTimer.innerHTML = minutes + ":" + seconds;
+}
+
+function timeToPercent() {
+    return (1 - (studyTime / (studyMinutes * 60)));
+}
+
+function percentToOffset(percent) {
+    return 1159 - 1159 * percent;
 }
